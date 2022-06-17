8305177b7a43dd64d8744977bb649e6cecb47344
lino202 <maxi.rosales202@gmail.com>
2022-06-17 18:42:52 +0200

    Add files for get data for Myriam to do deformation from exvivo to invivo ventricles

--- miscellaneous/utils.py
+++ miscellaneous/utils.py
@@ -33,3 +33,10 @@ def printUsefulStats(data):
     print("Useful Data: mean-std {0} +/- {1}".format(np.mean(data), np.std(data)))
     print("\t median {0}, lowQuart {1}, upQuart {2}, lowWhisker {3}, upWhisker {4}".format(boxplotData[0], boxplotData[1], boxplotData[2], boxplotData[3], boxplotData[4]))
 
+
+def writeFibers4JSON(filePath, rbmVersors):
+    with open(filePath, "w") as file:
+        file.write('"fibers":[[{0:.15f}, {1:.15f}, {2:.15f}],\n'.format(rbmVersors[0,0], rbmVersors[0,1], rbmVersors[0,2]))
+        for i in range(1,rbmVersors.shape[0]-1):
+            file.write("\t\t\t[{0:.15f}, {1:.15f}, {2:.15f}],\n".format(rbmVersors[i,0], rbmVersors[i,1], rbmVersors[i,2]))
+        file.write("\t\t\t[{0:.15f}, {1:.15f}, {2:.15f}]]".format(rbmVersors[-1,0], rbmVersors[-1,1], rbmVersors[-1,2]))
