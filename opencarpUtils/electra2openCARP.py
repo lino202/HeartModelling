c70ba32827de342d0708b53e4479d7c369446d7b
lino202 <maxi.rosales202@gmail.com>
2023-01-26 10:58:50 +0100

    Added opencarp utils

---
+++ opencarpUtils/electra2openCARP.py
@@ -0,0 +1,65 @@
+import meshio
+import argparse
+import os
+import numpy as np
+import utils as ut
+
+parser = argparse.ArgumentParser(description="Options")
+parser.add_argument('--filePath', type=str, required=True)
+parser.add_argument('--unit', type=str, required=True)
+parser.add_argument('--cellType', type=str, default="tetra")
+parser.add_argument('--stimPointData', type=str)
+parser.add_argument('--fibersPointData', type=str)
+parser.add_argument('--layersPointData', type=str)
+parser.add_argument('--outFolder', type=str, required=True)
+args = parser.parse_args()
+
+mesh = meshio.read(args.filePath)
+fileName = args.filePath.split(".")[0].split("/")[-1]
+#Switch to um as opencarp only seems to support that
+if args.unit=="cm":
+    mesh.points = mesh.points * 10000
+elif args.unit=="mm":
+    mesh.points = mesh.points * 1000
+elif args.unit=="um":
+    pass
+else: raise ValueError("Only units in cm, mm or um are supported")
+
+#Save pts in carp format
+print("Getting points as .pts file-------------")
+ut.writePointsFile(mesh.points, os.path.join(args.outFolder, "{}.pts".format(fileName)))
+
+#Save cells in carp format
+print("Getting cells as .elem file-------------")
+ut.writeCellsFile(mesh.cells_dict[args.cellType], os.path.join(args.outFolder, "{}.elem".format(fileName)), cellType=ut.meshioOCCellsMap[args.cellType])
+
+#Create .vtx files of point data of interest
+if hasattr(args, "stimPointData"):
+    print("Getting stim nodes as vtx file -------------")
+    tmp = mesh.point_data[args.stimPointData]
+    tmp = (tmp==1).nonzero()[0]
+    ut.writeStimVtxFile(tmp, os.path.join(args.outFolder, "{}.vtx".format(args.stimPointData)))
+
+#Add fibers to cell data
+# if fibers are already or you want just default fibers from meshtool just dont pass the argument
+if hasattr(args, "fibersPointData"):
+    print("Getting fibers for elem as .lon file-------------")
+    fibsCells = np.mean(mesh.point_data["fibers"][mesh.cells_dict["tetra"]], axis=1)
+    fibsCells = ut.getArrNormalization(fibsCells)
+    ut.writeFibsFile(fibsCells, os.path.join(args.outFolder, "{}.lon".format(fileName)))
+
+#Add tag to cell data for meshtool to understand
+# if hasattr(args, "layersPointData"):
+    # print("Getting cells tags in the vtk file -------------")
+    # fibsCells = np.mean(mesh.point_data["fibers"][mesh.cells_dict["tetra"]], axis=1)
+    # fibsCells = getArrNormalization(fibsCells)
+    # mesh.cell_data["fibers"] = [fibsCells]
+    # writeFlag = 1
+
+# We are not using meshtool as meshio saves fibs in FIELD data and meshtool wants them in VECTOR (need more research)
+# #Convert mesh to carp format
+# print("Converting mesh to CARP format")
+# cmdMesh = 'meshtool convert -imsh={0} -omsh={1}/{2}'.format(args.filePath, args.outFolder, args.filePath.split(".")[0].split("/")[-1])
+# outMesh = os.system(cmdMesh)
+# if outMesh==0: print("Mesh conversion successfully") 
+# else: print("Mesh conversion failure")
---
+++ opencarpUtils/utils.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+meshioOCCellsMap = {"tetra": "Tt"}
+
+def writeStimVtxFile(arr, outName, stimType="Intra"):
+    with open(outName, 'w') as f:
+        f.write("{0:d}\n".format(arr.shape[0]))
+        f.write("{}\n".format(stimType))                         #For now it seems ok to only have Intra
+        vtxs = list(map(lambda x: "{:d}\n".format(x),list(arr)))
+        f.writelines(vtxs)
+
+def getArrNormalization(arr):
+    if arr.shape[1]!=3: raise ValueError("Only work with nx3 arrays, for 3D vecs normalization")
+    arrMag = np.expand_dims(np.linalg.norm(arr, axis=1), axis=1)
+    return arr / np.hstack((arrMag,arrMag,arrMag))
+
+def writeFibsFile(arr, outName, nFibs=1):
+        with open(outName, 'w') as f:
+            f.write("{0:d}\n".format(nFibs))
+            if nFibs==1:
+                fibs = list(map(lambda x: "{0:f} {1:f} {2:f}\n".format(x[0], x[1], x[2]),list(arr)))
+            else:
+                raise ValueError("Wrong fibs direction number") 
+            f.writelines(fibs)
+
+
+def writePointsFile(arr, outName):
+        with open(outName, 'w') as f:
+            f.write("{0:d}\n".format(arr.shape[0]))
+            pts = list(map(lambda x: "{0:f} {1:f} {2:f}\n".format(x[0], x[1], x[2]),list(arr)))
+            f.writelines(pts)
+
+
+def writeCellsFile(arr, outName, cellType="Tt", tags=1):
+        with open(outName, 'w') as f:
+            f.write("{0:d}\n".format(arr.shape[0]))
+            if cellType=="Tt":
+                cells = list(map(lambda x: "{0:s} {1:d} {2:d} {3:d} {4:d} {5:d}\n".format(cellType, x[0], x[1], x[2], x[3], tags),list(arr)))
+            else: raise ValueError("Wrong cell type")
+            f.writelines(cells)
+
+
